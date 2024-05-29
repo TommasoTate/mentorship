8f50c80ec5e6812464ad40a8bf35aefdcbd845c9
Tommaso <65722261+TommasoTate@users.noreply.github.com>
2024-05-29 08:59:15 +0200

    removed unused imports

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,34 +1,6 @@
-import {
-  ClerkProvider,
-  SignInButton,
-  SignedIn,
-  SignedOut,
-  UserButton,
-} from "@clerk/nextjs";
+import {ClerkProvider} from "@clerk/nextjs";
 import "./globals.css";
-import {Tooltip, TooltipContent, TooltipProvider, TooltipTrigger} from "@/components/ui/tooltip";
-import Link from 'next/link'
-import {
-  Home,
-  LineChart,
-  Package,
-  Package2,
-  PanelLeft,
-  Search,
-  Settings,
-  ShoppingCart,
-  Users2
-} from 'lucide-react'
-import {Sheet, SheetContent, SheetTrigger} from '@/components/ui/sheet'
-import {Button} from '@/components/ui/button'
-import {
-  Breadcrumb,
-  BreadcrumbItem,
-  BreadcrumbLink,
-  BreadcrumbList, BreadcrumbPage,
-  BreadcrumbSeparator
-} from '@/components/ui/breadcrumb'
-import {Input} from '@/components/ui/input'
+import {TooltipProvider} from "@/components/ui/tooltip";
 
 export default function RootLayout({
   children,
