a6b986bcf3d830757fc85572e4c98005c4c76144
Tommaso <65722261+TommasoTate@users.noreply.github.com>
2024-06-16 16:57:15 +0200

    added timestamps

--- src/db/schema.ts
+++ src/db/schema.ts
@@ -1,4 +1,4 @@
-import {text, pgTable, pgEnum, serial, integer} from "drizzle-orm/pg-core";
+import {text, pgTable, pgEnum, serial, integer, timestamp} from "drizzle-orm/pg-core";
 
 export const startupStatusEnum = pgEnum("startupStatus", [
   "pending",
@@ -19,11 +19,18 @@ export const rolesEnum = pgEnum("roles", [
     "mentor"
     ]);
 
+const timpestampFields = {
+    createdAt: timestamp("createdAt").notNull().defaultNow(),
+    updatedAt: timestamp("updatedAt").notNull().defaultNow()
+        .$onUpdate(() => new Date())
+}
+
 export const users = pgTable("users", {
     id: serial("id").primaryKey(),
     email: text("email").notNull(),
     name: text("name").notNull(),
     role: rolesEnum("role").notNull(),
+    ...timpestampFields
 });
 
 export const startups = pgTable("startups", {
@@ -31,6 +38,7 @@ export const startups = pgTable("startups", {
   name: text("name").notNull(),
   description: text("description").notNull(),
   status: startupStatusEnum("status").notNull().default("pending"),
+    ...timpestampFields
 });
 
 export const consultations = pgTable("consultations", {
@@ -40,11 +48,13 @@ export const consultations = pgTable("consultations", {
   startDate: text("startDate").notNull(),
   endDate: text("endDate").notNull(),
   status: consultationStatusEnum("status").notNull().default("pending"),
+    ...timpestampFields
 });
 
 export const startupMembers = pgTable("startup_members", {
   startupperId: integer("startupperId").notNull().references(() => users.id),
-  startupId: integer("startupId").notNull().references(() => startups.id)
+  startupId: integer("startupId").notNull().references(() => startups.id),
+      ...timpestampFields
 });
 
 export type Startup = typeof startups.$inferSelect;
